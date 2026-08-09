bb6315681b7e5db0af98963798c35bb96ec112ff
yadhu <yadhukrishnanc83@gmail.com>
2026-08-09 14:13:22 +0530

    toggle

--- portfolio.js
+++ portfolio.js
@@ -1,5 +1,16 @@
 const form = document.querySelector('form');
 form.addEventListener('submit' , function(event){
     event.preventDefault();
-    console.log('form submit blocked');
+
+    form.style.display='none';
+    const message = document.createElement('p');
+    message.textContent = ' Thanks! your message has been sent.';
+    form.parentElement.appendChild(message);
+});
+
+const menuToggle = document.querySelector('#menu-toggle');
+const navList = document.querySelector('nav.links ul');
+
+menuToggle.addEventListener('click', function() {
+    navList.classList.toggle('open');
 });
